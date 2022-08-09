a2cc3e9991548bf929d7a384bacdb57ed21e67a9
Carson Blin <carsonb551@gmail.com>
2022-08-09 17:54:48 -0400

    Created passport.js WIP

---
+++ volunteer-management-system/passport.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
+
+const cookieExtractor = req =>{
+    let token = null;
+    if(req && req.cookies){
+        token = req.cookies["access_token"];
+    }
+    return token;
+}
+
+
+
+passport.use(new JwtStrategy({
+    jwtFromRequest : cookieExtractor,
+    secretOrKey : "Secret"
+}, (payload,done)=>{
+    //Check if user exists
+
+
+}
+
+
+
+passport.use(new LocalStrategy((username,password,done)=>{
+
+
+
+}))
