4f7068b3289ea90f3a6bbd6d2b75ccfb72360224
Miranda Weathers <mirandaweathers@gmail.com>
2022-08-11 18:50:08 -0400

    sql formatting for readability

--- volunteer-management-system/server/models/tableGeneration.js
+++ volunteer-management-system/server/models/tableGeneration.js
@@ -7,7 +7,10 @@ const checkForTables = (con) => {
 	});
 
 	con.query(
-		"CREATE TABLE IF NOT EXISTS USER (userId VARCHAR(255) PRIMARY KEY, password VARCHAR(255) NOT NULL, isAdmin BOOL)",
+		"CREATE TABLE IF NOT EXISTS USER (\
+				userId VARCHAR(255) PRIMARY KEY, \
+				password VARCHAR(255) NOT NULL, \
+				isAdmin BOOL)",
 		function (err) {
 			if (err) throw err;
 			//console.log("User Table Created");
@@ -15,12 +18,24 @@ const checkForTables = (con) => {
 	);
 
 	con.query(
-		"CREATE TABLE IF NOT EXISTS VOLUNTEER (volunteerId VARCHAR(255) PRIMARY KEY, firstName VARCHAR(255) NOT NULL, \
-        lastName VARCHAR(255) NOT NULL, address VARCHAR(255), homePhone VARCHAR(255),	workPhone VARCHAR(255),	cellPhone VARCHAR(255),	\
-        email VARCHAR(255),	education VARCHAR(255),	licenses VARCHAR(255), emContactName VARCHAR(255), emContactPhone VARCHAR(255),	\
-        emContactEmail VARCHAR(255), emContactAddress VARCHAR(255), \
-        driversLicense BOOL, socialSecurity BOOL, approvalStatus VARCHAR(255) NOT NULL,	\
-        skills VARCHAR(255), FOREIGN KEY(volunteerId) REFERENCES USER(userId) )",
+		"CREATE TABLE IF NOT EXISTS VOLUNTEER (\
+			volunteerId VARCHAR(255) PRIMARY KEY, \
+			firstName VARCHAR(255) NOT NULL, \
+        	lastName VARCHAR(255) NOT NULL, \
+			address VARCHAR(255), homePhone VARCHAR(255), \
+			workPhone VARCHAR(255),	cellPhone VARCHAR(255),	\
+       		email VARCHAR(255), \
+			education VARCHAR(255),	\
+			licenses VARCHAR(255), \
+			emContactName VARCHAR(255), \
+			emContactPhone VARCHAR(255), \
+        	emContactEmail VARCHAR(255),\
+			 emContactAddress VARCHAR(255), \
+        	driversLicense BOOL, \
+			socialSecurity BOOL, \
+			approvalStatus VARCHAR(255) NOT NULL, \
+        	skills VARCHAR(255), \
+			FOREIGN KEY(volunteerId) REFERENCES USER(userId))",
 		function (err) {
 			if (err) throw err;
 			//console.log("Volunteer Table Created");
@@ -36,8 +51,11 @@ const checkForTables = (con) => {
 	);
 
 	con.query(
-		"CREATE TABLE IF NOT EXISTS PREFERREDCENTER (volId VARCHAR(255) NOT NULL,\
-        ctrName VARCHAR(255) NOT NULL, FOREIGN KEY(volId) REFERENCES USER(userId), FOREIGN KEY(ctrName) REFERENCES CENTER(centerName)  )",
+		"CREATE TABLE IF NOT EXISTS PREFERREDCENTER (\
+			volId VARCHAR(255) NOT NULL, \
+        	ctrName VARCHAR(255) NOT NULL, \
+			FOREIGN KEY(volId) REFERENCES USER(userId), \
+			FOREIGN KEY(ctrName) REFERENCES CENTER(centerName))",
 		function (err) {
 			if (err) throw err;
 			//console.log("PREFFEREDCENTER Table Created");
@@ -45,8 +63,11 @@ const checkForTables = (con) => {
 	);
 
 	con.query(
-		"CREATE TABLE IF NOT EXISTS OPPORTUNITY (	ctrName VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, \
-        time DATETIME NOT NULL, FOREIGN KEY(ctrName) REFERENCES CENTER(centerName) )",
+		"CREATE TABLE IF NOT EXISTS OPPORTUNITY (\
+			ctrName VARCHAR(255) NOT NULL, \
+			category VARCHAR(255) NOT NULL, \
+        	time DATETIME NOT NULL, \
+			FOREIGN KEY(ctrName) REFERENCES CENTER(centerName))",
 		function (err) {
 			if (err) throw err;
 			//console.log("Opportunity Table Created");
