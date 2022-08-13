4af2d6c67dba5e7caf07680a540efaa5eb1021bf
Parker Hudson <gphudson97@hotmail.com>
2022-08-13 18:03:04 -0400

    VolunteerService proof of concept

--- volunteer-management-system/client/src/Components/ManageVolunteers/VolunteerController.js
+++ volunteer-management-system/client/src/Components/ManageVolunteers/VolunteerController.js
@@ -1,1 +1,25 @@
 //Main container to hold other components
+
+import React, { useState, useEffect } from "react";
+import VolunteerService from "../../Services/VolunteerService";
+
+const VolunteerController = () => {
+	const [volunteers, setVolunteers] = useState([]);
+
+	useEffect(() => {
+		VolunteerService.getVolunteers().then((data) => {
+			setVolunteers(data);
+		});
+	});
+
+	return (
+		<>
+			<h2>This is a placeholder for Volunteer Controller</h2>
+			<br />
+			<h1>List of Volunteers Test</h1>
+			<ul></ul>
+		</>
+	);
+};
+
+export default VolunteerController;
