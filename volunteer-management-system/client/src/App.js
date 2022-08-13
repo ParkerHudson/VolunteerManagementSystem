088154b019d24b666d6c5d1f63e0cd40eac87315
Carson Blin <carsonb551@gmail.com>
2022-08-13 14:03:38 -0400

    added authentication aspects

--- volunteer-management-system/client/src/App.js
+++ volunteer-management-system/client/src/App.js
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "./Context/AuthContext";
 import Home from "./Components/Home";
 
 function App() {
-	return <Home />;
+  const { user, setUser, isAuthenticated, setIsAuthenticated } =
+    useContext(AuthContext);
+  console.log(user);
+  console.log(isAuthenticated);
+  return <Home />;
 }
 
 export default App;
