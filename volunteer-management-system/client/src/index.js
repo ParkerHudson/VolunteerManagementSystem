23d93e9b04180045d514440afac3e265e1a0d12f
Carson Blin <carsonb551@gmail.com>
2022-08-13 14:04:01 -0400

    imported AuthProvider

--- volunteer-management-system/client/src/index.js
+++ volunteer-management-system/client/src/index.js
@@ -6,10 +6,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import $ from "jquery";
 import Popper from "popper.js";
+import AuthProvider from "./Context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
