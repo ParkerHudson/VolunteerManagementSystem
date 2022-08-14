0e331b816f9688b03a470932689435f107087631
Carson Blin <carsonb551@gmail.com>
2022-08-14 17:30:33 -0400

    Still working on editvolunteer

--- volunteer-management-system/client/src/Components/ManageVolunteers/EditVolunteer.js
+++ volunteer-management-system/client/src/Components/ManageVolunteers/EditVolunteer.js
@@ -3,3 +3,17 @@
 //Allow user to edit data and save to db
 //Delete volunteer button
 //back button to return to previous view
+
+//WIP
+import React from "react";
+
+const EditVolunteer = (props) => {
+	return (
+		<>
+			{props.volunteer.username}
+            
+		</>
+	);
+};
+
+export default EditVolunteer;
