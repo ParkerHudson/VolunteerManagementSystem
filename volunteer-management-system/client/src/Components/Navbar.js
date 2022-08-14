6e9dd8e48778ca4105fcd5c3b52f0d6a11586bf0
Carson Blin <carsonb551@gmail.com>
2022-08-14 17:24:55 -0400

    Created NavBar

--- volunteer-management-system/client/src/Components/Navbar.js
+++ volunteer-management-system/client/src/Components/Navbar.js
@@ -1,2 +1,71 @@
 //Logo to return to main menu.
 //logout button to return to login screen
+
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import AuthService from "../Services/AuthService";
+import { AuthContext } from "../Context/AuthContext";
+
+const Navbar = props => {
+    const unauthenticatedNavBar = () => {
+      return (
+        <>
+          <Link to="/Home">
+            <li className="nav-item nav-link">Home Page</li>
+          </Link>
+
+          <Link to="/Login">
+            <li className="nav-item nav-link">Login</li>
+          </Link>
+        </>
+      )
+    }
+
+  const authenticatedNavBar = () => {
+    return (
+      <>
+        <Link to="/Home">
+          <li className="nav-item nav-link">Home Page</li>
+        </Link>
+
+        <Link to="/Login">
+          <li className="nav-item nav-link">Login</li>
+        </Link>
+        <button
+          type="button"
+          className="btn btn-link nav-item nav-link"
+          onClick={onClickLogoutHandler}
+        >
+          Logout{" "}
+        </button>
+      </>
+    );
+  };
+
+  const onClickLogoutHandler = () => {
+    AuthService.logout().then((data) => {
+      if (data.success) {
+        setUser(data.user)
+      setIsAuthenticated(false)
+      }
+    });
+  };
+
+  const { isAuthenticated, user, setIsAuthenticated, setUser } =
+    useContext(AuthContext);
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <Link to="/">
+        <div className="navbar-brand">Admin Dashboard</div>
+      </Link>
+    
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                {!isAuthenticated ? unauthenticatedNavBar() : authenticatedNavBar()}
+          </ul>
+        </div>
+    </nav>
+  );
+};
+
+export default Navbar;
