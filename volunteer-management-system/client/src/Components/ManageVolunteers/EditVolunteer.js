3be2fdb12a0c0897b857aecb22f7161317931732
Parker Hudson <gphudson97@hotmail.com>
2022-08-14 22:01:29 -0400

    Added link to edit volunteer

--- volunteer-management-system/client/src/Components/ManageVolunteers/EditVolunteer.js
+++ volunteer-management-system/client/src/Components/ManageVolunteers/EditVolunteer.js
@@ -6,12 +6,15 @@
 
 //WIP
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const EditVolunteer = (props) => {
+	const location = useLocation();
+	const { volunteer } = location.state;
+	//volunteer is the volunteer object that is being passed to the component from the row component
 	return (
 		<>
-			{props.volunteer.username}
-            
+			<h1>{volunteer.username}</h1>
 		</>
 	);
 };
--- volunteer-management-system/client/src/Components/ManageVolunteers/VolunteerController.js
+++ volunteer-management-system/client/src/Components/ManageVolunteers/VolunteerController.js
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 const VolunteerController = () => {
 	const [volunteers, setVolunteers] = useState([]);
 
+	//Get volunteers from DB
 	useEffect(() => {
 		VolunteerService.getVolunteers().then((data) => {
 			setVolunteers(data);
