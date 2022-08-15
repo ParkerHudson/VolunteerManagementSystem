e46d9f845f5a36be89a657af114bbf69d3a12243
Miranda Weathers <mirandaweathers@gmail.com>
2022-08-15 10:18:00 -0400

    changed 'for' to 'htmlFor' in form labels

--- volunteer-management-system/client/src/Components/ManageVolunteers/AddVolunteer.js
+++ volunteer-management-system/client/src/Components/ManageVolunteers/AddVolunteer.js
@@ -2,6 +2,7 @@
 import React from "react";
 //import { stripBasename } from "react-router/lib/router";
 import { useState } from "react";
+import VolunteerService from "../../Services/VolunteerService";
 
 /*const handleSubmit = (e) => {
 
@@ -44,13 +45,14 @@ const AddVolunteer = (props) => {
 
 		//update database with form data
 	};
+	
 	return (
 		<form onSubmit={handleSubmit}>
 			<div className="form-inner">
 				<h2> Add a Volunteer </h2>
 
 				<div className="form-group">
-					<label for="firstName">First Name: </label>
+					<label htmlFor="firstName">First Name: </label>
 					<input
 						type="text"
 						value={name}
@@ -59,64 +61,64 @@ const AddVolunteer = (props) => {
 					/>
 				</div>
 				<div className="form-group">
-					<label for="lastName">Last Name: </label>
+					<label htmlFor="lastName">Last Name: </label>
 					<input type="text" name="lastName" id="lastName" required />
 				</div>
 				<div className="form-group">
-					<label for="address">Address: </label>
+					<label htmlFor="address">Address: </label>
 					<input type="text" name="address" id="address" required />
 				</div>
 				<div className="form-group">
-					<label for="homePhone">Home Phone: </label>
+					<label htmlFor="homePhone">Home Phone: </label>
 					<input type="text" name="homePhone" id="homePhone" required />
 				</div>
 				<div className="form-group">
-					<label for="workPhone">Work Phone: </label>
+					<label htmlFor="workPhone">Work Phone: </label>
 					<input type="text" name="workPhone" id="workPhone" required />
 				</div>
 				<div className="form-group">
-					<label for="cellPhone">Cell Phone: </label>
+					<label htmlFor="cellPhone">Cell Phone: </label>
 					<input type="text" name="cellPhone" id="cellPhone" required />
 				</div>
 				<div className="form-group">
-					<label for="email">Email: </label>
+					<label htmlFor="email">Email: </label>
 					<input type="text" name="email" id="email" required />
 				</div>
 				<div className="form-group">
-					<label for="education">Education: </label>
+					<label htmlFor="education">Education: </label>
 					<input type="text" name="education" id="education" required />
 				</div>
 				<div className="form-group">
-					<label for="licenses">Licenses: </label>
+					<label htmlFor="licenses">Licenses: </label>
 					<input type="text" name="licenses" id="licenses" required />
 				</div>
 				<div className="form-group">
-					<label for="emConName">Emergency Contact Name: </label>
+					<label htmlFor="emConName">Emergency Contact Name: </label>
 					<input type="text" name="emConName" id="emConName" required />
 				</div>
 				<div className="form-group">
-					<label for="emConPhone">Emergency Contact Phone: </label>
+					<label htmlFor="emConPhone">Emergency Contact Phone: </label>
 					<input type="text" name="emConPhone" id="emConPhone" required />
 				</div>
 				<div className="form-group">
-					<label for="emConEmail">Emergency Contact Email: </label>
+					<label htmlFor="emConEmail">Emergency Contact Email: </label>
 					<input type="text" name="emConEmail" id="emConEmail" required />
 				</div>
 				<div className="form-group">
-					<label for="emConAddress">Emergency Contact Address: </label>
+					<label htmlFor="emConAddress">Emergency Contact Address: </label>
 					<input type="text" name="emConAddress" id="emConAddress" required />
 				</div>
 				<div className="form-group">
-					<label for="dlNumber">Driver's License Number: </label>
+					<label htmlFor="dlNumber">Driver's License Number: </label>
 					<input type="text" name="dlNumber" id="dlNumber" required />
 				</div>
 				<div className="form-group">
-					<label for="ssn">Social Security Number: </label>
+					<label htmlFor="ssn">Social Security Number: </label>
 					<input type="text" name="ssn" id="ssn" required />
 				</div>
 
 				<div className="form-group">
-					<label for="skills">Skills: </label>
+					<label htmlFor="skills">Skills: </label>
 					<input type="text" name="skills" id="skills" />
 				</div>
 
