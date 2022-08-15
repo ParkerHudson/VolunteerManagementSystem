2522699a1c9ebe0f065c672d63f07a4e1d05c019
Parker Hudson <gphudson97@hotmail.com>
2022-08-15 03:36:02 -0400

    Fixed CSS imports

--- volunteer-management-system/client/src/App.js
+++ volunteer-management-system/client/src/App.js
@@ -5,6 +5,7 @@ import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 import EditVolunteer from "./Components/ManageVolunteers/EditVolunteer";
 import AddVolunteer from "./Components/ManageVolunteers/AddVolunteer";
+import VolunteerMatches from "./Components/ManageVolunteers/VolunteerMatches";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import VolunteerController from "./Components/ManageVolunteers/VolunteerController";
 
@@ -17,6 +18,7 @@ function App() {
 				<Route path="/addVolunteer" element={<AddVolunteer />} />
 				<Route path="/editVolunteer" element={<EditVolunteer />} />
 				<Route path="/manageVolunteers" element={<VolunteerController />} />
+				<Route path="/volunteerMatches" element={<VolunteerMatches />} />
 			</Routes>
 		</Router>
 	);
--- volunteer-management-system/client/src/index.js
+++ volunteer-management-system/client/src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./css/index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import $ from "jquery";
-import Popper from "popper.js";
-import AuthProvider from "./Context/AuthContext";
+import "./css/index.css";
+import "./css/App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
