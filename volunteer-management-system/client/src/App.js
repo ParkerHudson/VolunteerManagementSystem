36ea9e3f3327d2ad10f2a2dfdc7f9fb1847fde06
Parker Hudson <gphudson97@hotmail.com>
2022-08-14 22:02:13 -0400

    Added routes to editVolunteer and addVolunteer

--- volunteer-management-system/client/src/App.js
+++ volunteer-management-system/client/src/App.js
@@ -3,8 +3,10 @@ import React, { useContext } from "react";
 import { AuthContext } from "./Context/AuthContext";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
+import EditVolunteer from "./Components/ManageVolunteers/EditVolunteer";
 import AddVolunteer from "./Components/ManageVolunteers/AddVolunteer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import VolunteerController from "./Components/ManageVolunteers/VolunteerController";
 
 function App() {
 	return (
@@ -13,6 +15,8 @@ function App() {
 			<Routes>
 				<Route exact path="/" element={<Home />} />
 				<Route path="/addVolunteer" element={<AddVolunteer />} />
+				<Route path="/editVolunteer" element={<EditVolunteer />} />
+				<Route path="/manageVolunteers" element={<VolunteerController />} />
 			</Routes>
 		</Router>
 	);
