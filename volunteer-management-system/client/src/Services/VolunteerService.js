18aeb78a60acdce5564bb0654b37c03364401943
Miranda Weathers <mirandaweathers@gmail.com>
2022-08-16 17:02:39 -0400

    added volunteer skill functionality

--- volunteer-management-system/client/src/Services/VolunteerService.js
+++ volunteer-management-system/client/src/Services/VolunteerService.js
@@ -55,5 +55,74 @@ export default {
 		}).then((response) => {
 			return response.json().then((data) => data);
 		});
+	},
+
+	// addVolunteerSkill : fetch addSkill api
+	// add entry to skills table with username and skill
+	addVolunteerSkill: (volunteer, skill) => {
+		var volSkill = {};
+		volSkill.username = volunteer.username;
+		volSkill.skill = skill;
+
+		return fetch("/api/addSkill", {
+			method: "post",
+			body: JSON.stringify(volSkill),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}).then((response) => {
+			return response.json().then((data) => data);
+		});
+	},
+
+	// getVolunteerSkills : fetch getSkills api
+	// take in volunteer, return list of skills by username
+	getVolunteerSkills: (volunteer) => {
+		return fetch("/api/getSkills", {
+			method: "get",
+			body: JSON.stringify(volunteer),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}).then((response) => {
+			return response.json().then((data) => data);
+		});
+	},
+
+	// updateVolunteerSkills : fetch updateSkill api
+	// update skills table with volunteer username, skill (old), and newSkill
+	updateVolunteerSkill: (volunteer, skill, newSkill) => {
+		var update = {
+			username: volunteer.username,
+			newSkill: newSkill,
+			skill: skill
+		};
+
+		return fetch("/api/updateSkill", {
+			method: "post",
+			body: JSON.stringify(update),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}).then((response) => {
+			return response.json().then((data) => data);
+		});
+	},
+
+	deleteVolunteerSkill: (volunteer, skill) => {
+		var removeSkill = {
+			username: volunteer.username,
+			skill: skill
+		};
+
+		return fetch("/api/deleteSkill", {
+			method: "delete",
+			body: JSON.stringify(removeSkill),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}).then((response) => {
+			return response.json().then((data) => data);
+		});
 	}
 };
