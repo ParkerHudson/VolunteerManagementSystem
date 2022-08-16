a411c159eb6623abfc3be120b59e189a1b7cc81b
Parker Hudson <gphudson97@hotmail.com>
2022-08-16 19:45:05 -0400

    Add / edit volunteer updates

--- volunteer-management-system/client/src/Components/ManageVolunteers/AddVolunteer.js
+++ volunteer-management-system/client/src/Components/ManageVolunteers/AddVolunteer.js
@@ -54,43 +54,45 @@ const AddVolunteer = (props) => {
 	const [ssn, setSSN] = useState("");
 	const [skills, setSkills] = useState("");
 
-
-
-
 	const addVol = () => {
-	VolunteerService.postVolunteer({
-		username: `${firstName}`,
-		firstName: `${firstName}`,
-		lastName: `${lastName}`,
-		address: `${address}`,
-		homePhone: `${homePhone}`,
-		workPhone: `${workPhone}`,
-		cellPhone: `${cellPhone}`,
-		email: `${email}`,
-		education: `${education}`,
-		licenses: `${licenses}`,
-		emContactName: `${emContactName}`,
-		emContactPhone: `${emContactPhone}`,
-		emContactEmail: `${emContactEmail}`,
-		emContactAddress: `${emContactAddress}`,
-		driversLicense: 1,
-		socialSecurity: 1,
-		approvalStatus: `${firstName}`,
-		skills: `${skills}`,
-	});
-	console.log("Added volunteer.")
-}
-
-
-
+		VolunteerService.postVolunteer({
+			username: firstName,
+			firstName: firstName,
+			lastName: lastName,
+			address: address,
+			homePhone: homePhone,
+			workPhone: workPhone,
+			cellPhone: cellPhone,
+			email: email,
+			education: education,
+			licenses: licenses,
+			emContactName: emContactName,
+			emContactPhone: emContactPhone,
+			emContactEmail: emContactEmail,
+			emContactAddress: emContactAddress,
+			driversLicense: 1,
+			socialSecurity: 1,
+			approvalStatus: "approved",
+			skills: skills,
+		});
+		console.log("Added volunteer.");
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		alert(`${firstName}`);
+		addVol();
+		window.alert("Volunteer Added!");
 
 		//update database with form data
 	};
-	
+
+	//Approval status select
+	//Username field
+	//drivers license field (1 or 0)
+	//social security field (1 or 0)
+
+	//Skills checkboxes based off of center pref skills
+
 	return (
 		<form onSubmit={handleSubmit}>
 			<div className="form-inner">
@@ -107,127 +109,143 @@ const AddVolunteer = (props) => {
 				</div>
 				<div className="form-group">
 					<label htmlFor="lastName">Last Name: </label>
-					<input 
-					type="text" 
-					value = {lastName} 
-					onChange={(e) => setLastName(e.target.value)}
-					required />
+					<input
+						type="text"
+						value={lastName}
+						onChange={(e) => setLastName(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="address">Address: </label>
-					<input 
-					type="text"
-					 value={address}
-					 onChange={(e) => setAddress(e.target.value)} 
-					 required />
+					<input
+						type="text"
+						value={address}
+						onChange={(e) => setAddress(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="homePhone">Home Phone: </label>
-					<input 
-					type="text" 
-					value={homePhone}
-					onChange={(e) => setHomePhone(e.target.value)} 
-					required />
+					<input
+						type="text"
+						value={homePhone}
+						onChange={(e) => setHomePhone(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="workPhone">Work Phone: </label>
-					<input type="text"
-					value={workPhone}
-					onChange={(e) => setWorkPhone(e.target.value)}
-					
-					required />
+					<input
+						type="text"
+						value={workPhone}
+						onChange={(e) => setWorkPhone(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="cellPhone">Cell Phone: </label>
-					<input type="text"  
-					value={cellPhone}
-					onChange={(e) => setCellPhone(e.target.value)}
-					
-					required />
+					<input
+						type="text"
+						value={cellPhone}
+						onChange={(e) => setCellPhone(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="email">Email: </label>
-					<input type="text"  
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-					
-					required />
+					<input
+						type="text"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="education">Education: </label>
-					<input type="text"  
-					value={education}
-					onChange={(e) => setEducation(e.target.value)}
-					
-					required />
+					<input
+						type="text"
+						value={education}
+						onChange={(e) => setEducation(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="licenses">Licenses: </label>
-					<input type="text" 
-					value={licenses}
-					onChange={(e) => setLicenses(e.target.value)}
-					
-					required />
+					<input
+						type="text"
+						value={licenses}
+						onChange={(e) => setLicenses(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="emConName">Emergency Contact Name: </label>
-					<input type="text" 
-					value={emContactName}
-					onChange={(e) => setEmContactName(e.target.value)}
-					
-					required />
+					<input
+						type="text"
+						value={emContactName}
+						onChange={(e) => setEmContactName(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="emConPhone">Emergency Contact Phone Number: </label>
-					<input type="text" 
-					value={emContactPhone}
-					onChange={(e) => setEmContactPhone(e.target.value)}
-					
-					required />
+					<input
+						type="text"
+						value={emContactPhone}
+						onChange={(e) => setEmContactPhone(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="emConEmail">Emergency Contact Email: </label>
-					<input type="text" 
-					value={emContactEmail}
-					onChange={(e) => setEmContactEmail(e.target.value)}
-					
-					required />
+					<input
+						type="text"
+						value={emContactEmail}
+						onChange={(e) => setEmContactEmail(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="emConAddress">Emergency Contact Address: </label>
-					<input type="text"  
-					value={emContactAddress}
-					onChange={(e) => setEmContactAddress(e.target.value)}
-					
-					required />
+					<input
+						type="text"
+						value={emContactAddress}
+						onChange={(e) => setEmContactAddress(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="dlNumber">Driver's License Number: </label>
-					<input type="text"  
-					value={dlNumber}
-					onChange={(e) => setDLNumber(e.target.value)}
-					
-					required />
+					<input
+						type="text"
+						value={dlNumber}
+						onChange={(e) => setDLNumber(e.target.value)}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="ssn">Social Security Number: </label>
-					<input type="text"  
-					value={ssn}
-					onChange={(e) => setSSN(e.target.value)}
-					
-					required />
+					<input
+						type="text"
+						value={ssn}
+						onChange={(e) => setSSN(e.target.value)}
+						required
+					/>
 				</div>
 
 				<div className="form-group">
 					<label htmlFor="skills">Skills: </label>
-					<input type="text"  
-					value={skills}
-					onChange={(e) => setSkills(e.target.value)}
-					
+					<input
+						type="text"
+						value={skills}
+						onChange={(e) => setSkills(e.target.value)}
 					/>
 				</div>
 
-				<button type="button" class="btn btn-success" onClick={addVol}>Submit</button>
+				<button type="button" class="btn btn-success" onClick={addVol}>
+					Submit
+				</button>
 			</div>
 		</form>
 	);
