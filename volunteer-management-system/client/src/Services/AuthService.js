0263fdedea6868de7323792e1fbfa897ed99da92
Parker Hudson <gphudson97@hotmail.com>
2022-08-17 04:45:12 -0400

    Authentification API and services

--- volunteer-management-system/client/src/Services/AuthService.js
+++ volunteer-management-system/client/src/Services/AuthService.js
@@ -10,15 +10,18 @@ export default {
 			.then((res) => res.json())
 			.then((data) => data);
 	},
-	logout: () => {
-		return fetch("/user/logout")
-			.then((res) => res.json())
-			.then((data) => data);
-	},
-	isAuthenticated: () => {
+
+	isAuthenticated: (token) => {
 		return fetch(
-			"/user/authenticated".then((res) => {
-				if (res.status !== 401) return res.json().then((data) => data);
+			"user/authenticated",
+			{
+				method: "post",
+				body: JSON.stringify(user),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}.then((res) => {
+				if (res.isAuthorized == "true") return res.json().then((data) => data);
 				else return { isAuthenticated: false, user: { username: "" } };
 			})
 		);
