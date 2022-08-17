13530276c52a38ad9d67ce815b601c0caa410968
Miranda Weathers <mirandaweathers@gmail.com>
2022-08-17 12:36:46 -0400

    added opportunity service methods

--- volunteer-management-system/client/src/Services/OpportunityService.js
+++ volunteer-management-system/client/src/Services/OpportunityService.js
@@ -1,5 +1,98 @@
 const Opportunity = require("../../../server/models/Opportunity");
 
-let postOpportunity = (ctrName, category, time) => {
-	var opp = {};
-};
+export default {
+	// addOpp : fetch addOpportunity api
+	// take in center name, category, and time
+	addOpp: (ctrName, category, time) => {
+		var opp = {
+			ctrName: ctrName,
+			category: category,
+			time: time
+		}
+
+		return fetch("api/addOpportunity", {
+			method: "post",
+			body: JSON.stringify(opp),
+			headers: {
+				"Content-Type": "application/json"
+			},
+		}).then((response) => {
+			return response.json().then((data) => data);
+		});
+	},
+
+	// getOpps : fetch getOpportunities api
+	// return all opportunities, with filter and search options
+	getOpps: (filter, search) => {
+		var query = new URLSearchParams();
+		if (filter != "") {
+			query.append("filter", filter);
+		} else {
+			query.append("filter", "all");
+		}
+		if (search != "") {
+			query.append("search", search);
+		}
+
+		let url = "/api/getOpportunities?" + query;
+		return fetch(url).then((response) => {
+			return response.json().then((data) => data);
+		});
+	},
+
+	// updateOpp : fetch updateOpportunity api
+	// take in opportunity, update name/category/time for oppID
+	updateOpp: (opportunity) => {
+		return fetch("/api/updateOpportunity", {
+			method: "put",
+			body: JSON.stringify(opportunity),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}).then((response) => {
+			return response.json().then((data) => data);
+		});
+	},
+
+	// deleteOpp : fetch deleteOpportunity api
+	// take in opportunity, delete entry for oppID
+	deleteOpp: (opportunity) => {
+		return fetch("/api/deleteOpportunity", {
+			method: "delete",
+			body: JSON.stringify(opportunity),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}).then((response) => {
+			return response.json().then((data) => data);
+		});
+	},
+
+	// getOpportunityMatches : fetch getOppMatches api
+	// take in username, return list of matching opportunities
+	getOpportunityMatches: (volunteer) => {
+		return fetch("api/getOppMatches", {
+			method: "get",
+			body: JSON.stringify(volunteer),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}).then((response) => {
+			return response.json().then((data) => data);
+		});
+	},
+
+	// getVolunteerMatches : fetch getVolMatches api
+	// take in oppID, return list of matching volunteers
+	getVolunteerMatches: (opportunity) => {
+		return fetch("api/getVolMatches", {
+			method: "get",
+			body: JSON.stringify(opportunity),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}).then((response) => {
+			return response.json().then((data) => data);
+		});
+	}
+}
